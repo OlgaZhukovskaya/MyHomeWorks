ac772fedafed2ced455b527e03e3f470f188fabd
Olga Zhukovskaya <ozhukovskaya@mail.ru>
2025-08-09 17:04:28 +0300

    My first home work on java

--- src/HomeWork2/TV.java
+++ src/HomeWork2/TV.java
@@ -8,7 +8,24 @@ public class TV {
     private int diagonal;
     private String podsvenka;
 
+    void displayInfo(){
+        System.out.printf(marka, diagonal, podsvenka);
+    }
+
+    public TV(String marka, int diagonal, String podsvenka){
+        this.marka= marka;
+        this.diagonal= diagonal;
+        this.podsvenka = podsvenka;
+    }
+
     public static void main (String[] args){
+        TV TvSet = new TV("HOSHIBA", 33, "НЕТ");
+        System.out.println(TvSet.marka);
+        System.out.println(TvSet.diagonal);
+        System.out.println(TvSet.podsvenka);
+    }
+
+    /*public static void main (String[] args){
         TV TvSet = new TV();
         TvSet.marka = "Samsung";
         TvSet.diagonal = 24;
@@ -16,5 +33,5 @@ public class TV {
 
         System.out.println("Создан телефизор марки " + TvSet.marka + " с диагональю " + TvSet.diagonal);
         System.out.println("Подсветка: "+ TvSet.podsvenka);
-    }
+    }*/
 }
